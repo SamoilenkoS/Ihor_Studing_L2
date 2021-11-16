89ccaa5500b972f1190cb39d55f95ba41174623d
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-11-16 18:53:26 +0200

    Add project files.

---
+++ Ihor_Studing_L2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ihor_Studing_L2
+{
+    class Program
+    {
+        static double Task1(double A, double B) // A and B
+        {
+            return (5 * A + B * B) / (B - A);
+        }
+
+        static void Task2(ref string a, ref string b) // Changing string
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static (int, int) Task3(int a, int b) // Remainder
+        {
+            return (a / b, a % b);
+        }
+
+        static double Task4(double a, double b, double c) // Not equal to 0
+        {
+            return (c - b) / a;
+        }
+
+        static (double, double) Task5(double x1, double y1, double x2, double y2)
+        {
+            double a = (y1 - y2) / (x1 - x2);
+            double b = y2 - a * x2;
+
+            return (a, b);
+        }
+
+        static void Main()
+        {
+            string a = "Hello";
+            string b = "World";
+            Task2(ref a, ref b);
+
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
